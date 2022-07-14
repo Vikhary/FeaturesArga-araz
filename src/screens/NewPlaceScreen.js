b4c8ab82802e48eefcbbea06252be72eb9b93ca0
Vic Arga99 <vickyarga99@gmail.com>
2022-07-14 19:39:25 -0300

    Primer Commit

---
+++ src/screens/NewPlaceScreen.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import { ScrollView, View, StyleSheet, Text, TextInput, Button } from "react-native";
+import colors from "../utils/colors";
+import { useDispatch } from "react-redux";
+import { savePlace } from "../store/place.slice";
+import ImageSelector from "../components/ImageSelector";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  content: {
+    flex: 1,
+    margin: 20,
+  },
+  title: {
+    fontSize: 18,
+    marginBottom: 20
+  },
+  input: {
+    borderBottomColor: colors.primary,
+    borderBottomWidth: 1,
+    marginBottom: 20,
+    padding: 5,
+  }
+});
+
+const NewPlaceSreen = ({ navigation }) => {
+  const dispatch = useDispatch();
+  const [title, setTitle] = useState("");
+  const [image, setImage] = useState("");
+
+  const onHandleTitleChange = (text) => setTitle(text);
+  const onHandleSubmit = () => {
+    dispatch(savePlace(title, image));
+    navigation.navigate("Place");
+  }
+
+  const onHandleImaageSelect = (imageUrl) => setImage(imageUrl);
+
+  return (
+    <ScrollView style={styles.container}>
+     <View style={styles.content}>
+        <Text style={styles.title}>Titulo</Text>
+        <TextInput style={styles.input} placeholder="Nueva ubicacion"  onChangeText={onHandleTitleChange} value={title}/>
+        <ImageSelector onImage={onHandleImaageSelect} />
+        <Button 
+          title="Grabar Direccion"
+          color={colors.primary}
+          onPress={onHandleSubmit}
+        />
+     </View>
+    </ScrollView>
+  );
+};
+
+export default NewPlaceSreen;
---
+++ src/store/place.slice.js
@@ -0,0 +1,43 @@
+import { createSlice } from "@reduxjs/toolkit";
+import * as FileSystem from "expo-file-system";
+import Place from "../models/Place";
+
+const initialState = {
+  places: [],
+};
+
+const placeSlice = createSlice({
+  name: "place",
+  initialState,
+  reducers: {
+    addPlace: (state, action) => {
+      const newPlace = new Place(Date.now(), action.payload.title, action.payload.image);
+      state.places.push(newPlace);
+    }
+  },
+});
+
+export const { addPlace } = placeSlice.actions;
+
+export const savePlace =   (title, image) => {
+  return async (dispatch) => {
+    const fileName = image.split("/").pop();
+    const Path = FileSystem.documentDirectory + fileName;
+
+    console.log('Path', Path);
+
+    try {
+      await FileSystem.moveAsync({
+        from: image,
+        to: Path,
+      });
+    } catch (error) {
+      console.log(error.message);
+      throw error;
+    }
+
+    dispatch(addPlace({ title, image: Path }));
+  }
+  }
+
+export default placeSlice.reducer;
