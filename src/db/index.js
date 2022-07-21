6adda5305c47ce734cac4fd12ea56ed3b8dec111
Vic Arga99 <vickyarga99@gmail.com>
2022-07-20 21:33:38 -0300

    Segundo Commit

---
+++ src/db/index.js
@@ -0,0 +1,40 @@
+import * as SQLite from 'expo-sqlite'
+
+const db = SQLite.openDatabase('address.db');
+
+export const init = () => {
+    const promise = new Promise((resolve, reject) => {
+        db.transaction(tx => {
+            tx.executeSql(
+                'CREATE TABLE IF NOT EXISTS address (id INTEGER PRIMARY KEY NOT NULL, title TEXT NO NULL, image TEXT NOT NULL)',
+                [],
+                () => resolve(),
+                (_, err) => reject(err)
+            )
+        })
+    })
+
+    return promise;
+}
+
+export const insertAddress = (title, image) => {
+    const promise = new Promise((resolve, reject)=> {
+        db.transaction(tx => {
+            tx.executeSql(
+                'INSERT INTO address (title, image) VALUES (?, ?)',
+                [title, image, ],
+                (_, result) => {
+                    console.log(result);
+                    resolve(result)
+                },
+                (_, err) => {
+                    console.log(err);
+                    reject(err)
+                }
+            )
+        })
+
+    })
+
+    return promise;
+}
--- src/store/place.slice.js
+++ src/store/place.slice.js
@@ -1,6 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 import * as FileSystem from "expo-file-system";
 import Place from "../models/Place";
+import { insertAddress } from "../db";
 
 const initialState = {
   places: [],
@@ -11,7 +12,7 @@ const placeSlice = createSlice({
   initialState,
   reducers: {
     addPlace: (state, action) => {
-      const newPlace = new Place(Date.now(), action.payload.title, action.payload.image);
+      const newPlace = new Place(action.payload.id.toString(), action.payload.title, action.payload.image);
       state.places.push(newPlace);
     }
   },
@@ -19,24 +20,30 @@ const placeSlice = createSlice({
 
 export const { addPlace } = placeSlice.actions;
 
-export const savePlace =   (title, image) => {
+export const savePlace =   ( title, image ) => {
   return async (dispatch) => {
+    let result;
     const fileName = image.split("/").pop();
     const Path = FileSystem.documentDirectory + fileName;
 
-    console.log('Path', Path);
+
+
+  
 
     try {
       await FileSystem.moveAsync({
         from: image,
         to: Path,
       });
+
+     result = await insertAddress (title, Path);
+      console.log('result insertAddress', result)
     } catch (error) {
       console.log(error.message);
       throw error;
     }
 
-    dispatch(addPlace({ title, image: Path }));
+    dispatch(addPlace({ id: result.insertId, title, image: Path}));
   }
   }
 
